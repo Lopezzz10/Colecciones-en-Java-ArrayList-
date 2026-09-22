3f7a7a187abe296650830b021bad00a79b53d7a7
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-09-22 16:56:58 -0500

    Método_Buscar Contacto

--- src/com/krakedev/contacto/entidades/Directorio.java
+++ src/com/krakedev/contacto/entidades/Directorio.java
@@ -40,4 +40,13 @@ public class Directorio {
 		String numero = c.getCelular();		
 		return numero;
 	}
+	public Contacto buscarConctato (String numero) {
+		for(int i = 0; i < contactos.size(); i++) {
+			Contacto c =  contactos.get(i);
+			if(c.getCelular().equals(numero)) {
+				return c;
+			}
+		}
+		return  null;
+	}
 }
