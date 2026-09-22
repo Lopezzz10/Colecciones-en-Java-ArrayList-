985d2ee9e9f9dfb7258ecbb62c15bd92009c0543
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-09-21 20:55:09 -0500

    IndexOutOfBoundsException

---
+++ src/com/krakedev/contacto/entidades/test/TestReferenciaYErrores.java
@@ -0,0 +1,17 @@
+package com.krakedev.contacto.entidades.test;
+
+import com.krakedev.contacto.entidades.Contacto;
+import com.krakedev.contacto.entidades.Directorio;
+
+public class TestReferenciaYErrores {
+
+	public static void main(String[] args) {
+		Directorio dir = new Directorio();
+		Contacto c1 = new Contacto();
+		c1.setNombre("Maria");
+		dir.agregarContacto(c1);
+		dir.agregarContacto(new Contacto());
+		Contacto c = dir.obtenerContacto(2);
+		System.out.println("Nombre: "+c.getNombre());
+	}
+}
