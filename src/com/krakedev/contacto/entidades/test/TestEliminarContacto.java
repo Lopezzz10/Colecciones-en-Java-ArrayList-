6a4b4810b37777b3a2bdeda3f3c1e3b7bdbe2c22
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-09-22 19:13:21 -0500

    Test_Eliminar Contacto

---
+++ src/com/krakedev/contacto/entidades/test/TestEliminarContacto.java
@@ -0,0 +1,29 @@
+package com.krakedev.contacto.entidades.test;
+
+import com.krakedev.contacto.entidades.Contacto;
+import com.krakedev.contacto.entidades.Directorio;
+
+public class TestEliminarContacto {
+
+	public static void main(String[] args) {
+		Directorio dir = new Directorio();
+		Contacto c1 = new Contacto();
+		c1.setNombre("Maria");
+		c1.setCelular("0912345678");
+		Contacto c2 = new Contacto();
+		c2.setNombre("Juan");
+		c2.setCelular("4912343673");
+		Contacto c3 = new Contacto();
+		c3.setNombre("Carlos");
+		c3.setCelular("9922342673");
+		dir.agregarContacto(c1);
+		dir.agregarContacto(c2);
+		dir.agregarContacto(c3);
+		boolean r1 = dir.eliminarContacto("0912345678");
+		System.out.println("Resultado de eliminar contacto 1: "+r1);
+		System.out.println("Cantidad de contactos: "+ dir.obtenerCantidadContactos());
+		boolean r2 = dir.eliminarContacto("0912345678");
+		System.out.println("Resultado de eliminar contacto 1: "+r2);
+	}
+
+}
